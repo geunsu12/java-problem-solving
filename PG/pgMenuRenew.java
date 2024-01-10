76a75af81a1fc241b6f455197db0637b753ef197
geunsu <geunsu23@naver.com>
2024-01-10 17:21:44 +0900

    feat: 프로그래머스 문제 풀이 추가

---
+++ PG/pgMenuRenew.java
@@ -0,0 +1,52 @@
+package PG;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+public class pgMenuRenew {
+
+    HashMap<String, Integer> map;
+    
+    public String[] solution(String[] orders, int[] course) {
+        
+        ArrayList<String> answerList = new ArrayList<>();
+        
+        for (int courseLength : course) {
+            map = new HashMap<>();
+            for (String order : orders) {
+                String[] tmpOrder = order.split("");
+                Arrays.sort(tmpOrder);
+                String sortedOrder = String.join("",tmpOrder);
+                combination(sortedOrder,"",0,courseLength);
+                
+            }
+            
+            int maxCount = 0;
+            for (int value : map.values()) {
+                maxCount = Math.max(maxCount, value);
+            }
+            
+            if (maxCount >= 2) {
+                for (Entry<String, Integer> entrySet : map.entrySet()) {
+                    if (entrySet.getValue() == maxCount) {
+                        answerList.add(entrySet.getKey());
+                    }
+                }
+            }
+            
+        }
+        Collections.sort(answerList);
+        return answerList.toArray(new String[answerList.size()]);
+    }
+    
+    public void combination(String order, String tmpCourse, int depth, int length) {
+        if(depth == length) {
+            map.put(tmpCourse, map.getOrDefault(tmpCourse,0)+1);
+        }
+        else {
+            for(int i=0;i<order.length();i++) {
+                combination(order.substring(i+1,order.length()),tmpCourse+order.charAt(i),depth+1,length);
+            }
+        }
+    }
+}
